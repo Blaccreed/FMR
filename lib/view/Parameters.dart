a8f78af368402d5d5a2b6cbc57ec63107f5373c6
Blaccreed <62603834+Blaccreed@users.noreply.github.com>
2022-06-08 10:25:18 +0200

    FINITO

--- lib/view/Parameters.dart
+++ lib/view/Parameters.dart
@@ -40,6 +40,7 @@ class _ParametersState extends State<Parameters> {
 
     return SafeArea(
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         backgroundColor: HexColor("#0000ff"),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
